38ffacafd05fc3e312c3dd4b382e81ee80c6b7eb
Marko Ilic <19marko91.ilic@gmail.com>
2023-11-23 13:13:53 +0100

    Adding models and binding components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is a test recipe', 'https://www.foodandwine.com/thmb/dX7pNh_WX83ESqb9VJuvkBwVKwM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg'),
+    new Recipe('Chicken Pasta', 'This is a Chicken pasta', 'https://skinnyspatula.com/wp-content/uploads/2022/10/Creamy_Garlic_Chicken_Pasta_0-720x720.jpg')
+  ];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
