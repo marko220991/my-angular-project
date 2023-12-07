9e34cf4ed05eade3ab5266b732485c4ec417616e
Marko Ilic <19marko91.ilic@gmail.com>
2023-12-07 13:13:27 +0100

    Pass dinamic parameters using routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRouter: Routes = [
   {path: '', redirectTo: '/recipe', pathMatch: 'full'},
   {path: 'recipe', component: RecipesComponent, children: [
-    {path: '', component: RecipeStartComponent}
+    {path: '', component: RecipeStartComponent},
+    {path: ':id', component: RecipeDetailComponent}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]
