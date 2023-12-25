d39b52f657d42f4760a05ee8a117521b2f25dc49
Marko Ilic <19marko91.ilic@gmail.com>
2023-12-25 14:54:52 +0100

    Improve error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
   kind: string;
@@ -18,12 +20,24 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>
-    ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAZKAJH6NUepxbU7mPjkTxAcizuJyKWmhc', 
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    });
+      return this.http.post<AuthResponseData>
+      ('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAZKAJH6NUepxbU7mPjkTxAcizuJyKWmhc', 
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
+        }
+      ).pipe(catchError(errorRes => {
+        let errorMessage = 'An errorOccured!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email address already exists!';
+        }
+        return throwError(errorMessage);
+      })
+    );
   }
 }
