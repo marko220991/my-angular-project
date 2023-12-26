f3722dd407631a8511e799b0ae7620da2be6ac7a
Marko Ilic <19marko91.ilic@gmail.com>
2023-12-26 11:03:35 +0100

    Route user after login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+
+  private userSub: Subscription;
+  isAuth = false;
+
   @Output() featureSelected = new EventEmitter<string>();
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuth = !!user;
+    });
+  }
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
@@ -20,5 +33,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchData().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
